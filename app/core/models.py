a29a5a7f544279a82478f86912c0847d963a2102
Michail Kravchenko <66253910+kravchenmd@users.noreply.github.com>
2022-12-28 19:12:53 +0100

    Implement normalization of email

--- app/core/models.py
+++ app/core/models.py
@@ -16,7 +16,8 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
-        user = self.model(email=email, **extra_fields)  # eq to callling User
+        user = self.model(email=self.normalize_email(email),
+                          **extra_fields)  # eq to callling User
         user.set_password(password)
         user.save(using=self._db)
 
