2584bbe4e02398f1df149bd29b37bf5627ed7a16
Michail Kravchenko <66253910+kravchenmd@users.noreply.github.com>
2023-01-02 17:19:46 +0100

    Create test for creting ingredient and implement the model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -82,3 +82,13 @@ class ModelTetst(TestCase):
         tag = models.Tag.objects.create(user=user, name='Tag1')
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_create_ingridient(self):
+        """Test creating an ingredient is successful."""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='Ingredient1',
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
