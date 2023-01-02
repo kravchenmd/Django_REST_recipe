59b9a18ea63e73b078cee02e539f761057c15b83
Michail Kravchenko <66253910+kravchenmd@users.noreply.github.com>
2023-01-02 12:28:31 +0100

    Add tests for updating tags and implement feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -38,7 +38,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.UpdateModelMixin,
+                 mixins.ListModelMixin, viewsets.GenericViewSet):
     """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
@@ -48,4 +49,3 @@ class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     def get_queryset(self):
         """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
-
