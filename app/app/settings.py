53510f981ff766552d7ed863db881bd8b868b5be
Michail Kravchenko <66253910+kravchenmd@users.noreply.github.com>
2023-01-02 23:04:59 +0100

    Implement functionality for uploading images via recipe endpoint

--- app/app/settings.py
+++ app/app/settings.py
@@ -143,3 +143,8 @@ AUTH_USER_MODEL = 'core.User'  # set auth of user configuration
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
+# for uploading images through browsable interface
+SPECTACULAR_SETTINGS = {
+    'COMPONENT_SPLIT_REQUEST': True,
+}
