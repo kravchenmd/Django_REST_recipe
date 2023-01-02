a3a9fd3d6ba9377341c6510f8c2c233082ce4cf2
Michail Kravchenko <66253910+kravchenmd@users.noreply.github.com>
2023-01-02 22:19:17 +0100

    Edit recipe model to store uploaded images and make migration

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,9 @@
 """
 Database models.
 """
+import uuid
+import os
+
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
@@ -10,6 +13,14 @@ from django.contrib.auth.models import (
 )
 # AbstractBaseUser: Functionality for auth system
 # PermissionsMixin: Functionality for the permissions & fields
+
+
+def recipe_image_file_path(instance, filename):
+    """Generates file path to upload a new recipe image."""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+
+    return os.path.join('uploads', 'recipe', filename)
 
 
 class UserManager(BaseUserManager):
@@ -62,6 +73,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self) -> str:
         return self.title
